51ecb7f87d031343129db0e7e787934f47aec05a
itsmac <saagarmac@gmail.com>
2021-12-29 00:24:51 +0530

    feat: added the user registration

--- MarkOne/MarkOne/settings.py
+++ MarkOne/MarkOne/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #New application
     'authapp.apps.AuthappConfig',
+    'rest_framework',
 
 ]
 
@@ -78,9 +79,14 @@ WSGI_APPLICATION = 'MarkOne.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	    'NAME':  'markone',
+        'HOST': 'localhost',
     }
 }
 
--- MarkOne/authapp/views.py
+++ MarkOne/authapp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
-
+class RegisterView(generics.CreateAPIView):
+    permission_class = (AllowAny,)
+    query_set = User.objects.all()
+    serializer_class = RegisterSerializer
